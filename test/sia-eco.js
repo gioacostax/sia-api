1fe7a61483e9a622861c18335961a339a0f9097e
gioacostax <gioacostax@gmail.com>
2017-01-28 10:26:58 -0500

    [Test] Restructure test files

---
+++ test/sia-eco.js
@@ -0,0 +1,4 @@
+/* eslint-env mocha */
+/* eslint no-unused-expressions: 0 */
+/* eslint no-var: 0 */
+/* eslint no-invalid-this: 0 */
--- test/sia.js
+++ test/sia.js
@@ -1,11 +1,7 @@
-'use strict';
-
 /* eslint-env mocha */
 /* eslint no-unused-expressions: 0 */
-/* eslint no-var: 0 */
-/* eslint no-invalid-this: 0 */
 
-const sia = require('../build/npm');
+const sia = require('../src/sia');
 const expect = require('chai').expect;
 
 describe('SIA', function callback() {
@@ -24,7 +20,7 @@ describe('SIA', function callback() {
   });
 
   describe('.getGroups()', () => {
-    var resGroups = [];
+    let resGroups = [];
 
     before(done => {
       sia.getGroups(2018484, 'sia.bogota.unal.edu.co').then(res => {
@@ -45,37 +41,5 @@ describe('SIA', function callback() {
     it('Limit Parse', () => {
       expect(resGroups[0]).to.have.deep.property('limits');
     });
-  });
-});
-
-describe('UTILS', () => {
-  var day = [
-    null,
-    { place: ['217-305'], hour: ['7-10'] },
-    null,
-    { place: ['217-302'], hour: ['7-10'] },
-    null,
-    null,
-    null
-  ];
-
-  var filter = ['M7', 'M8', 'M9', 'J7', 'J8', 'J9'];
-  var param = 'L:M7-10:C:J7-10:V:S:D';
-
-  it('.parseSchedule()', () => {
-    expect(sia.utils.parseSchedule(day)).to.deep.equal(filter);
-  });
-
-  it('.dayValidFilter()', () => {
-    expect(sia.utils.validFilter(['J9', 'M8'], filter)).to.be.true;
-    expect(sia.utils.validFilter(['J11', 'M8'], filter)).to.not.be.true;
-  });
-
-  it('.parseParam()', () => {
-    expect(sia.utils.parseParam(param)).to.deep.equal(filter);
-  });
-
-  it('.parseFilter()', () => {
-    expect(sia.utils.parseFilter(filter)).to.deep.equal(param);
   });
 });
